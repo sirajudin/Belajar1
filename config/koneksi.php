0a6f440c15be040effd18d7f88b23183a331107b
Sirajudin <Sirajudin@Sirajudin-PC>
2016-10-28 15:07:09 +0700

    Assalamualaikum First Program

--- config/koneksi.php
+++ config/koneksi.php
@@ -5,4 +5,16 @@
  * about java Progrmming To built a sistem 
  * 
  */
+$server="localhost";
+$user="root";
+$password="";
+$database="lesson";
+$koneksi=  mysqli_connect($server, $user, $password, $database);
+
+if ($koneksi){
+    echo"Berhasil koneksi";
+}
+else {
+    echo"gagal koneksi";
+}
 
